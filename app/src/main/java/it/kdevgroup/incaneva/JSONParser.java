aef7f1095a18455578212e5802701b93e12b8077
AndreaMinato <andrea.minato@hotmail.it>
2016-04-07 11:04:57 +0200

    Basi del metodo di parsing

--- app/src/main/java/it/kdevgroup/incaneva/JSONParser.java
+++ app/src/main/java/it/kdevgroup/incaneva/JSONParser.java
@@ -1,5 +1,12 @@
 package it.kdevgroup.incaneva;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
 /**
  * Created by damiano on 07/04/16.
  */
@@ -14,12 +21,24 @@ public class JSONParser {
 
     }
 
-    public void setStr(String str) {
+    public void setJsonBody(String str) {
         this.jsonBody = str;
     }
 
-    public String getStr() {
+    public String getJsonBody() {
 
         return jsonBody;
+    }
+
+    public List<BlogEvent> parseJson(){
+        JSONObject jsnobject = new JSONObject(jsonBody);
+        JSONArray jsonArray = jsnobject.getJSONArray("locations");
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject explrObject = jsonArray.getJSONObject(i);
+            }catch (Exception e){
+                Log.d("Cazzo: ", "Non va");
+            }
+        }
     }
 }
