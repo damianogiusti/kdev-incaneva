dbbbe9dd5be083e318fbc6c8c56d2095913cb901
AndreaMinato <andrea.minato@hotmail.it>
2016-04-07 10:50:24 +0200

    Stessa roba per JSONPARSER

--- app/src/main/java/it/kdevgroup/incaneva/JSONParser.java
+++ app/src/main/java/it/kdevgroup/incaneva/JSONParser.java
@@ -4,7 +4,22 @@ package it.kdevgroup.incaneva;
  * Created by damiano on 07/04/16.
  */
 public class JSONParser {
+    private String jsonBody;
 
+    public JSONParser(String str) {
+        this.jsonBody = str;
+    }
 
-    
+    public JSONParser() {
+
+    }
+
+    public void setStr(String str) {
+        this.jsonBody = str;
+    }
+
+    public String getStr() {
+
+        return jsonBody;
+    }
 }
