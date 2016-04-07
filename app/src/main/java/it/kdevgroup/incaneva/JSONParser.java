9a955aef4fb7dabca7c16e6252b2f2d6183f19f1
Damiano Giusti <damianogiusti95@gmail.com>
2016-04-07 11:46:23 +0200

    commentato jsonparser,  resi metodi statici

--- app/src/main/java/it/kdevgroup/incaneva/JSONParser.java
+++ app/src/main/java/it/kdevgroup/incaneva/JSONParser.java
@@ -14,29 +14,23 @@ import java.util.ListIterator;
 
 /**
  * Created by damiano on 07/04/16.
+ * But developed by andrea on the same date.
  */
 public class JSONParser {
-    public static final String DATA = "data";
-    private String jsonBody;
 
-    public JSONParser(String str) {
-        this.jsonBody = str;
-    }
+    public static final String DATA = "data";
 
     public JSONParser() {
-
     }
 
-    public void setJsonBody(String str) {
-        this.jsonBody = str;
-    }
-
-    public String getJsonBody() {
-
-        return jsonBody;
-    }
-
-    public List<BlogEvent> parseJson() throws Exception {
+    /**
+     * Effettua il parsing di una risposta json ottenuta dal server
+     *
+     * @param jsonBody risposta json ottenuta dal server
+     * @return lista di oggetti di tipo BlogEvent
+     * @throws Exception
+     */
+    public static List<BlogEvent> parseJsonResponse(String jsonBody) throws Exception {
         List<BlogEvent> eventi = new ArrayList<>();
         JSONObject jsnobject = new JSONObject(jsonBody);
         JSONArray jsonArray = jsnobject.getJSONArray(DATA);
@@ -44,11 +38,17 @@ public class JSONParser {
             JSONObject obj = jsonArray.getJSONObject(i);
             eventi.add(parseJsonObject(obj));
         }
-
         return eventi;
     }
 
-    public BlogEvent parseJsonObject(JSONObject obj) throws Exception {
+    /**
+     * Effettua il parsing di un singolo oggetto JSON
+     *
+     * @param obj JSONObject da parsare
+     * @return oggetto di tipo BlogEvent parsato
+     * @throws Exception
+     */
+    public static BlogEvent parseJsonObject(JSONObject obj) throws Exception {
         BlogEvent blogEvent = new BlogEvent();
         blogEvent.setBlogName(obj.getString(BlogEvent.KEY_blogname));
         blogEvent.setBlogNameSlug(obj.getString(BlogEvent.KEY_blogname_slug));
@@ -70,10 +70,7 @@ public class JSONParser {
         blogEvent.setEndTime(obj.getLong(BlogEvent.KEY_evcal_srow));
         blogEvent.setEventColor(obj.getString(BlogEvent.KEY_evcal_event_color));
 
-
         return blogEvent;
-
-
     }
 
 }
