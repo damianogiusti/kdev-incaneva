c42d78dfe31d1bb9d9638601a6bcddf93bd9e1d6
Mariana Anitoiu <nmarianapae@yahoo.it>
2016-04-11 16:21:58 +0200

    dettagli su dettagli

--- app/src/main/java/it/kdevgroup/incaneva/DetailActivity.java
+++ app/src/main/java/it/kdevgroup/incaneva/DetailActivity.java
@@ -10,10 +10,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DetailActivity extends AppCompatActivity {
 
     ImageView imgEvent;
-    TextView txtTitle, txtTipo, txtContent,txtStartEnd;
+    TextView txtTitle, txtTipo, txtContent, txtStartEnd, txtData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +28,35 @@ public class DetailActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        imgEvent = (ImageView)findViewById(R.id.imgEvent);
-        txtTitle = (TextView)findViewById(R.id.txtTitle);
-        txtTipo = (TextView)findViewById(R.id.txtTipo);
-        txtContent = (TextView)findViewById(R.id.txtContent);
-        txtStartEnd = (TextView)findViewById(R.id.txtStartEnd);
+        imgEvent = (ImageView) findViewById(R.id.imgEvent);
+        txtTitle = (TextView) findViewById(R.id.txtTitle);
+        txtTipo = (TextView) findViewById(R.id.txtTipo);
+        txtContent = (TextView) findViewById(R.id.txtContent);
+        txtStartEnd = (TextView) findViewById(R.id.txtStartEnd);
+        txtData = (TextView) findViewById(R.id.txtData);
+
+        Bundle vBundle = getIntent().getBundleExtra(HomeActivity.BUNDLE_KEY_FOR_ARRAY);
+
+        if (vBundle != null) {
+            // imgEvent = vBundle.getString()
+            txtTitle.setText(vBundle.getString(BlogEvent.KEY_post_title));
+            txtTipo.setText(vBundle.getString(BlogEvent.KEY_event_type));
+            txtContent.setText(vBundle.getString(BlogEvent.KEY_post_content));
+
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyy");
+            long dataInizio = vBundle.getLong(BlogEvent.KEY_evcal_srow) * 1000;
+            long dataFine = vBundle.getLong(BlogEvent.KEY_evcal_erow) * 1000;
+
+            String dataInizioFormat = sdf.format(new Date(dataInizio));
+            String dataFineFormat = sdf.format(new Date(dataFine));
+            if (dataInizio == dataFine){
+                txtStartEnd.setText(dataInizioFormat);
+            }else {
+                txtStartEnd.setText(dataInizioFormat + " - " + dataFineFormat);
+            }
+            //txtStartEnd.setText();//da finire
+           // txtData.setText(vBundle.getString(BlogEvent.));
+        }
 
     }
 
